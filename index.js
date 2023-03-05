460ad6ec40c7d1442a80a20ef86662ae09ca98a9
Christina Boribong <CPBoribong@gmail.com>
2023-03-05 18:54:16 -0500

    Completed Lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+var n = 10;
+function increment(n) {
+n++
+return n;
+}
+
+var m = 0;
+function decrement(m) {
+m--
+return m;
+
+}
+function makeInt(string) {
+    return parseInt(string,10);
+}
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
